0fa1edcaef5d7e909e780a42644e71ba265ad997
leegit <ljy0707521@163.com>
2018-04-14 16:59:30 +0800

    lee 页面整体（过渡效果和布局）

--- js/service.js
+++ js/service.js
@@ -14,4 +14,58 @@ function btn(){
       video.pause();
 
   })
+$(document).ready(function(){
 
+
+    $('.nav-pills').on('click',function(e){
+        $(e.target).parent().siblings().find('a').removeClass('active');
+        $(e.target).addClass("active");
+        var ss = $(e.target).data("filter");
+        var w =$('.img-responsive').width();
+        var h =$('.img-responsive').height();
+        if(ss=="all") {
+        }else if(ss ==".mockups"){
+            $('.mix').css('display', "none");
+            $('.mockups').css('display', "inline-block");
+            $('.mix').height(0);
+            $('.mix').width(0);
+            $('.mockups').height(h);
+            $('.mockups').width(w);
+        }else if(ss ==".icons"){
+            $('.mix').css('display', "none");
+            $('.mix').style.height="0px";
+            $('.icons').css('display', "inline-block");
+        }else if(ss==".templates"){
+            $('.mix').css('display', "none");
+            $('.mix').style.height="0px";
+            $('.templates').css('display', "inline-block");
+        }else{
+            $('.mix').css('display', "none");
+            $('.mix').style.height="0px";
+            $('.ui-kits').css('display', "inline-block");
+        }
+    })
+
+});
+
+     $('.projects').on('mouseenter','figure',function(e){
+        var target = $(e.target);
+         target.siblings('.mask').css('opacity','1');
+     })
+     $('.projects').on('mouseleave','figure',function(e){
+         var target = $(e.target);
+         target.siblings('.mask').css('opacity','0');
+     })
+
+
+$(window).bind('scroll', function () {
+        var th = $(window).scrollTop();
+        if(th > 100){
+            $('.navbar-default').addClass("navbar-small");
+        }else{
+            $('.navbar-default').removeClass("navbar-small");
+            console.log($(''));
+        }
+
+}
+)
